1cb470a6cf3e0416ea7af83e5c94247324824c86
Andreas Stuhlmüller <andreas@stuhlmueller.org>
2016-04-25 16:32:58 -0700

    Make mutable state explicit

--- src/library.js
+++ src/library.js
@@ -1,1 +1,38 @@
-console.log('library.js loaded.');
+var _ = require('underscore');
+
+
+function CacheState(obj) {
+  this.obj = obj;
+}
+
+CacheState.prototype = {
+  constructor: CacheState,
+  get: function(key) {
+    return this.obj[key];
+  },
+  set: function(key, value) {
+    this.obj[key] = value;
+  }
+};
+
+function CacheStates() {
+  this.states = [];
+}
+
+
+CacheStates.prototype = {
+  constructor: CacheStates,
+  add: function(obj) {
+    var state = new CacheState(obj);
+    this.states.push(state);
+    return state;
+  },
+  clear: function() {
+    this.states = [];
+  }
+};
+
+
+module.exports = {
+  cacheStates: new CacheStates()
+};
